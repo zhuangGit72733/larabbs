829aadd9499173502cda9407a5b053660309d195
zhuanggit <1437383705@qq.com>
2018-12-01 10:30:08 +0800

    List user replies

---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A
+ * Date: 2018/12/1
+ * Time: 10:20
+ */
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -64,5 +64,13 @@
                 </div>
             </div>
         </div>
+        {{-- 用户回复列表 --}}
+        <div class="panel panel-default topic-reply">
+            <div class="panel-body">
+                @include('topics._reply_box', ['topic' => $topic])
+                @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+            </div>
+        </div>
+
     </div>
 @stop
