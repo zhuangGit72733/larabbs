9ddb8a9c26e6a9d771406d43f307362f5a182efc
zhuanggit <1437383705@qq.com>
2018-12-04 16:13:04 +0800

    add easysms

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,9 @@ $api->version('v2', function ($api) {
         return response('this is version v2');
     });
 });
+//// 图片验证码
+//$api->post('captchas', 'CaptchasController@store')
+//    ->name('api.captchas.store');
+//// 第三方登录
+//$api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+//    ->name('api.socials.authorizations.store');
