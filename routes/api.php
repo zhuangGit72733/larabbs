a62a99289f7439b23e8ca137ea2139182cabda32
zhuanggit <1437383705@qq.com>
2018-12-05 10:14:49 +0800

    verificationCode store

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,43 +15,17 @@ use Illuminate\Http\Request;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+
+
 $api = app('Dingo\Api\Routing\Router');
-
-$api->version('v1', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v1');
-    });
-//    $api->resource('version2', function () {
-//       return response('this is resource');
-//    });
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api',
+], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
 });
 
-$api->version('v2', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v2');
-    });
-});
 
-//$api->group([
-//    'middleware' => 'api.throttle',
-//    'limit' => config('api.rate_limits.access.limit'),
-//    'expires' => config('api.rate_limits.access.expires'),
-//], function ($api) {
-//    // 游客可以访问的接口
-//
-//    // 需要 token 验证的接口
-//    $api->group(['middleware' => 'api.auth'], function ($api) {
-//        // 当前登录用户信息
-//        $api->get('user', 'UsersController@me')
-//            ->name('api.user.show');
-//    });
-//});
-//// 图片验证码
-//$api->post('captchas', 'CaptchasController@store')
-//    ->name('api.captchas.store');
-//// 第三方登录
-//$api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-//    ->name('api.socials.authorizations.store');
+
+
